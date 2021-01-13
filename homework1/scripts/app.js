368fbf887fec0f09fc9ae724c75001c1dffe60f5
Ivo Kostovski <ivo.kostovski@gmail.com>
2021-01-13 21:03:40 +0100

    Add files via upload

---
+++ homework1/scripts/app.js
@@ -0,0 +1,18 @@
+// FIND NUMBERS IN ARRAY
+// How many times is a number contained in an array
+// Write a function named findNumber that:
+// Takes 2 arguments: number, array
+// Calculates how many times the number is contained in an array with numbers
+// Outputs the result to the screen like so: "There is 4 occurrences of number 5 in the array"
+// Call the function three times with different arrays.
+
+
+let a = '';
+
+
+// FILTER ODD/EVEN NUMBERS FUNCTION
+// Write a function named findNumber that:
+// Takes 2 arguments: array, type
+// The person that calls the function should provide an array of numbers and string odd / even
+// If the type is even the function should find all the even numbers and return them as result
+// If the type is odd the function should find all the odd numbers and return them as result
